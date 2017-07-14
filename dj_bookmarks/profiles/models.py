3381fe8e73d9fd95b3401463ea7aed2714e5bd61
Maranino <admin@ambitiousfew.com>
2017-07-14 13:00:10 -0700

    Add profiles app

---
+++ dj_bookmarks/profiles/models.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+AUTH_USER_MODEL = get_user_model()
+
+def avatar_upload_path(instance, filename):
+    """
+    Builds the media path for where it will store
+    the avatar images.
+    """
+    return os.path.join('avatars', 'user_{0}', '{1}').format(
+        instance.user.id, filename)
+
+
+class UserProfile(models.Model):
+    """A Basic User Profile"""
+    user = models.OneToOneField(
+        AUTH_USER_MODEL,
+        primary_key=True,
+        related_name='profile'
+    )
+    first_name = models.CharField(
+        blank=True,
+        default='',
+        max_length=50
+    )
+    last_name = models.CharField(
+        blank=True,
+        default='',
+        max_length=50
+    )
+    avatar = models.ImageField(
+        'Avatar picture',
+        upload_to=avatar_upload_path,
+        null=True,
+        blank=True
+    )
+    bio = models.TextField("Short Bio", default='')
+
+    @property
+    def full_name(self):
+        if self.first_name:
+            return '{} {}'.format(self.first_name, self.last_name)
+        return '{}'.format(self.user.get_short_name())
+
+    @property
+    def get_avatar_url(self):
+        if self.avatar:
+            return '/media/{}'.format(self.avatar)
+        return '/media/default.jpg'
+
+    def __str__(self):
+        return "{}'s profile".format(self.user.get_username())
