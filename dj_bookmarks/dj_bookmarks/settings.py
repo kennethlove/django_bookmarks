2d02154d336265038d14d29026a51135f37963d7
Maranino <admin@ambitiousfew.com>
2017-07-14 13:22:39 -0700

    Add static, media paths to settings and urls for dev

--- dj_bookmarks/dj_bookmarks/settings.py
+++ dj_bookmarks/dj_bookmarks/settings.py
@@ -124,5 +124,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # URLs
 LOGOUT_REDIRECT_URL = 'login'
--- dj_bookmarks/dj_bookmarks/urls.py
+++ dj_bookmarks/dj_bookmarks/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
@@ -26,4 +28,10 @@ urlpatterns = [
     url(r'^accounts/', include('profiles.urls', namespace='accounts')),
     url(r'^admin/', admin.site.urls),
     url(r'^bookmarks/', include('bookmarks.urls', namespace='bookmarks')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
