d3b16e0e854a706fa5ead0991d05637aa73ebea2
Kenneth Love <kenneth@gigantuan.net>
2017-07-14 10:41:51 -0700

    stuff
    base layout, login and logout.
    
    closes #2

--- dj_bookmarks/dj_bookmarks/settings.py
+++ dj_bookmarks/dj_bookmarks/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'dj_bookmarks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# URLs
+LOGOUT_REDIRECT_URL = 'login'
--- dj_bookmarks/dj_bookmarks/urls.py
+++ dj_bookmarks/dj_bookmarks/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
