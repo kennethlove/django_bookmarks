b83f9754fa30f1907a683d6396c5eb70be3d9682
Maranino <admin@ambitiousfew.com>
2017-07-14 13:00:54 -0700

    Add profiles app to URLS and INSTALLED_APPS

--- dj_bookmarks/dj_bookmarks/settings.py
+++ dj_bookmarks/dj_bookmarks/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'bootstrap3',
     'authentication',
     'bookmarks',
+    'profiles',
 ]
 
 MIDDLEWARE = [
--- dj_bookmarks/dj_bookmarks/urls.py
+++ dj_bookmarks/dj_bookmarks/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^signup/$', SignUpView.as_view(), name='signup'),
+    url(r'^accounts/', include('profiles.urls', namespace='accounts')),
     url(r'^admin/', admin.site.urls),
     url(r'^bookmarks/', include('bookmarks.urls', namespace='bookmarks')),
 ]
